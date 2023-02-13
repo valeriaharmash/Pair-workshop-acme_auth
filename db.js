ba089cf5aa78086a4d0d21225c37491609bcc3c5
kbwong3 <brianwong0109@gmail.com>
2023-02-13 16:37:07 -0500

    finished bcrypt tab in lab. hashed all passwords before creation.

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
   logging: false,
@@ -11,11 +12,11 @@ if (process.env.LOGGING) {
   delete config.logging;
 }
 const conn = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost/acme_db',
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
   config
 );
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   username: STRING,
   password: STRING,
 });
@@ -28,29 +29,35 @@ User.byToken = async (token) => {
     if (user) {
       return user;
     }
-    const error = Error('bad credentials');
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
   } catch (ex) {
-    const error = Error('bad credentials');
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
   }
 };
 
+User.beforeCreate(async (user) => {
+  user.password = await bcrypt.hash(user.password, 10);
+  console.log("hash pass -->", user.password);
+});
+
 User.authenticate = async ({ username, password }) => {
   const user = await User.findOne({
     where: {
       username,
-      password,
     },
   });
-  // const token = jwt.sign(user.id, process.env.JWT);
+  console.log("INSIDE AUTHENTICATE (param) username", username);
+  console.log("INSIDE AUTHENTICATE (param) password", password);
+  console.log("INSIDE AUTHENTICATE user", user);
 
-  if (user) {
+  if (user && (await bcrypt.compare(password, user.password))) {
     return jwt.sign({ id: user.id }, process.env.JWT);
   }
-  const error = Error('bad credentials');
+  const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
@@ -58,9 +65,9 @@ User.authenticate = async ({ username, password }) => {
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw' },
-    { username: 'moe', password: 'moe_pw' },
-    { username: 'larry', password: 'larry_pw' },
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
   ];
   const [lucy, moe, larry] = await Promise.all(
     credentials.map((credential) => User.create(credential))
